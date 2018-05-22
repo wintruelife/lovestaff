c47be30ee6bb9451c2cb171356e8b4789dc20bcc
lnl1314abm <lnl1314abm>
2018-05-22 11:35:04 +0800

    测试

--- app/src/main/java/love/wintrue/com/lovestaff/MainActivity.java
+++ app/src/main/java/love/wintrue/com/lovestaff/MainActivity.java
@@ -2,12 +2,16 @@ package love.wintrue.com.lovestaff;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import love.wintrue.com.lovestaff.base.MApplication;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final String TAG = MApplication.class.getName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG,"测试版本控制");
     }
 }
