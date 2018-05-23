83b404443c683f4c97ff09f21e50b6fa0cd32c01
zhangwei <zhangwei>
2018-05-23 09:58:31 +0800

    mine
    test

---
+++ app/src/main/java/love/wintrue/com/lovestaff/ui/mine/MineActivity.java
@@ -0,0 +1,16 @@
+package love.wintrue.com.lovestaff.ui.mine;
+
+import android.os.Bundle;
+
+import love.wintrue.com.lovestaff.base.BaseActivity;
+
+/**
+ * Created by a1314 on 2018/5/23.
+ */
+
+public class MineActivity extends BaseActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
